7d338b6920c9c38a1da5bedf2cf85ca3f8b57738
UNIT Enterprise Systems <55563130+GLPG35@users.noreply.github.com>
2022-08-29 01:03:36 -0300

    v2.1.1
    - Fixed tests page for the needs of the mentor.

--- js/tests.js
+++ js/tests.js
@@ -1,18 +1,48 @@
 const calcSueldo = () => {
-    const nombre = prompt('Ingrese su nombre:')
-    const sueldo = Number(prompt('Ingrese su sueldo nominal:'))
-    const hijos = Number(prompt('Tiene hijos?\n\n1) Si\n2) No'))
+    let error = 0
 
-    // Cálculo de descuentos
-    const discBPS = calcImpuesto(sueldo, 18)
-    const discIRPF = calcImpuesto(sueldo, 15)
-    const discFONASA = hijos == 1 ? calcImpuesto(sueldo, 6) : calcImpuesto(sueldo, 4)
-    const discFRL = calcImpuesto(sueldo, 1.25)
+    do {
+        const nombre = prompt('Ingrese su nombre:')
 
-    // Sueldo líquido
-    const liquido = sueldo - discBPS - discIRPF - discFONASA - discFRL
+        if (nombre == null || nombre == '') {
+            error = 1
+        } else {
+            const sueldo = Number(prompt('Ingrese su sueldo nominal:'))
 
-    alert(`${nombre}, su sueldo líquido es de $${liquido}, gracias por permitirnos robarle su dinero`)
+            if (isNaN(sueldo)) {
+                error = 1
+            } else {
+                const hijos = Number(prompt('Tiene hijos?\n\n1) Si\n2) No'))
+
+                if (isNaN(hijos)) {
+                    error = 1
+                } else {
+                    // Cálculo de descuentos
+                    const discBPS = calcImpuesto(sueldo, 18)
+                    const discIRPF = calcImpuesto(sueldo, 15)
+                    let discFONASA
+
+                    if (hijos == 1) {
+                        discFONASA = calcImpuesto(sueldo, 6)
+                    } else {
+                        discFONASA = calcImpuesto(sueldo, 4)
+                    }
+
+                    const discFRL = calcImpuesto(sueldo, 1.25)
+
+                    // Sueldo líquido
+                    const liquido = sueldo - discBPS - discIRPF - discFONASA - discFRL
+
+                    alert(`${nombre}, su sueldo líquido es de $${liquido}, gracias por permitirnos robarle su dinero`)
+                    error = 0
+                }
+            }
+        }
+
+        if (error == 1) {
+            alert('Ha ocurrido un error, realice la operación nuevamente')
+        }
+    } while (error == 1)
 }
 
 const calcImpuesto = (sueldo, descuento) => {
